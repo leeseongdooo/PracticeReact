2918e98bb3a34a9e4154fa2b36f91037e26c2571
leeseongdooo <tjdgns8966@naver.com>
2022-05-24 12:30:44 +0900

    Day0524(2)

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
-  return (
+  const [Item, setItem] = useState("AA");
+
+  // Input을 업데이트
+  const useInput = (initialValue) => {
+    const [value, setValue] = useState(initialValue);
+
+    const onChange = (e) => {
+      setValue(e.target.value)
+      console.log(value)
+    }
+
+    return {onChange, value};
+  }
+
+  const name = useInput("MR");
+
+  return (   
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <input type="text" placeholder="name" onChange={name.onChange}/>
+          <p>{name.value}</p>
       </header>
     </div>
   );
